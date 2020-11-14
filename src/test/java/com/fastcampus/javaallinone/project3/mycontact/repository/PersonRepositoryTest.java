7edf953918cf17f5d32169ec75671b71f2e7b333
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-14 15:34:10 +0900

    feat : Implement Person's prototype entity & repository & test class

---
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.fastcampus.javaallinone.project3.mycontact.repository;
+
+import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PersonRepositoryTest {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud() {
+        Person person = new Person();
+        person.setName("martin");
+        person.setAge(10);
+
+        personRepository.save(person);
+
+//        System.out.println(personRepository.findAll());
+
+        List<Person> people = personRepository.findAll();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("martin");
+        assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+}
