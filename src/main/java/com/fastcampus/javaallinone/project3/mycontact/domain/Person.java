c2c8e688cb7dcb307f1d6b3e0488b788999070c4
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-14 16:35:18 +0900

    refactor : Change Person entity simpler

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,51 +1,42 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @NonNull
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDate birthDay;
 
-    public int getAge() {
-        return age;
-    }
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    @ToString.Exclude
+    private String phoneNumber;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
