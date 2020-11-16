9c958ba8cd0f6adf44d2c55dd31dc7c651c344b7
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-17 00:59:45 +0900

    refactor : Delete needless Min Max annotation in Birthday entity

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/dto/Birthday.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/dto/Birthday.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import java.time.LocalDate;
 
 @Embeddable
@@ -15,17 +13,17 @@ public class Birthday {
 
     private Integer yearOfBirthday;
 
-    @Min(1)
-    @Max(12)
     private Integer monthOfBirthday;
 
-    @Min(1)
-    @Max(31)
     private Integer dayOfBirthday;
 
-    public Birthday(LocalDate birthday) {
+    private Birthday(LocalDate birthday) {
         this.yearOfBirthday = birthday.getYear();
         this.monthOfBirthday = birthday.getMonthValue();
         this.dayOfBirthday = birthday.getDayOfMonth();
+    }
+
+    public static Birthday of(LocalDate birthday) {
+        return new Birthday(birthday);
     }
 }
