4b7058663a759f21a6db5760087eea71141dc7ec
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-16 22:53:20 +0900

    style : Delete comment line

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -17,24 +17,15 @@ public class PersonService {
     private PersonRepository personRepository;
 
     public List<Person> getPeopleExcludeBlocks() {
-//        List<Person> people = personRepository.findAll();
-
-//        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
-
         return personRepository.findByBlockIsNull();
     }
 
     public List<Person> getPeopleByName(String name) {
-//        List<Person> people = personRepository.findAll();
-
-//        return people.stream().filter(person -> person.getName().equals(name)).collect(Collectors.toList());
-
         return personRepository.findByName(name);
     }
 
     @Transactional(readOnly = true)
     public Person getPerson(Long id) {
-//        Person person = personRepository.findById(id).get();
         Person person = personRepository.findById(id).orElse(null);
 
         log.info("person : {}", person);
